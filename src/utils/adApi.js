20a423ca69a579530e04cf1f05b5bdc19a995386
Brahmakosh Team <brahmakoshtech@gmail.com>
2026-08-24 23:42:15 +0530

    first commit

--- src/utils/adApi.js
+++ src/utils/adApi.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+import { API_BASE_URL } from './constants'
 
 const adApi = axios.create({
   baseURL: `${API_BASE_URL}/api`,
